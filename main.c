6460506a52fbf3fc571519071f7a1b3e57da8309
Martin Kloeckner <mjkloeckner@gmail.com>
2025-08-30 15:33:06 -0300

    Reference the datasheet when defining the reg address

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
-// Port B input pins register address
+// (see sections 13.2.2 and 13.4 on datasheet)
 #define PINB (*(volatile unsigned char*) 0x23)
 
 int main(void) {
 
     while(1) {
         PINB |= (1 << 5);
-        for(volatile long i = 0; i < 500000L; ++i);
+
+        for(volatile long i = 0; i < 500000L; ++i)
+            ;
     }
 
     return 0;
